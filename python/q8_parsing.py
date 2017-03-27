7811a962a9ad543eb2973166c7f1cfbbac6be538
Mariya Petukhova <mariyapetukhova@Mariyas-MacBook-Pro.local>
2017-03-27 01:19:24 -0500

    work in progress

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -4,3 +4,24 @@
 # against opponents, and had 36 goals scored against them). Write a program to read the file, 
 # then print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
 
+import csv
+
+f=open('./football.csv', 'rb')
+reader=csv.reader(f)
+
+rownum = 0
+for row in reader:
+   if rownum == 0:
+      header = row
+   else:
+      colnum = 0
+for col in row:
+   colnum += 1
+   rownum +=1
+
+
+print (row)
+
+
+f.close()
+
