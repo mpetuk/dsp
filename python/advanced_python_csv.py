fcd50adfdbd4f6ade596434e39e0ee5d2beef33a
Mariya Petukhova <mariyapetukhova@Mariyas-MacBook-Pro.local>
2017-03-31 10:06:23 -0500

    done

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,15 @@
-PLACE YOUR CODE HERE
+import csv
+reader=csv.DictReader(open('faculty.csv'))
+
+emails=[]
+for row in reader:
+     e=row[" email"]
+     emails.append(e)
+
+print(emails)
+
+with open('emails.csv','w') as output:
+   writer=csv.writer(output,lineterminator='\n')
+   for val in emails:
+      writer.writerow([val])
+   
