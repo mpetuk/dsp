5e35794743a8b6ce07fb75dee339ff484c44dfbf
Mariya Petukhova <mariyapetukhova@Mariyas-MacBook-Pro.local>
2017-03-31 10:05:44 -0500

    done

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -3,6 +3,8 @@ reader=csv.DictReader(open('faculty.csv'))
 
 degrees=[]
 titles=[]
+emails=[]
+domains=[]
 p = re.compile(r'\W+')
 for row in reader:
   s=row[" degree"]
@@ -13,6 +15,17 @@ for row in reader:
   s21=re.sub(r'\bis\b','of',s1)
   titles.append(s21)
 
+  e=row[" email"]
+  emails.append(e)
+  
+  d=e[e.find("@")+1:]
+  domains.append(d)
+  
+  #Alternative way for domain accumulation
+  #d = re.search("@[\w.]+", e)
+  #d1= d.translate({ord("@"):None})
+  #domains.append(d.group()) 
+
 #print(degrees)
 import itertools
 one_list= filter(None,list(itertools.chain.from_iterable(degrees)))
@@ -20,10 +33,12 @@ one_list= filter(None,list(itertools.chain.from_iterable(degrees)))
 
 #print(one_list)
 from collections import Counter
-freqs=Counter(one_list)
-freqs2=Counter(titles)
+freqs_degrees=Counter(one_list)
+freqs_titles=Counter(titles)
+freqs_domains=Counter(domains)
 
-print(freqs)
-print(freqs2)
-
+print(freqs_degrees)
+print(freqs_titles)
+print(emails)
+print(freqs_domains)
 
